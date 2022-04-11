094e27b2ae510ed5d3a7ce9b0aaae859a64ab321
Diego_Marcolino <marcolinodiego.stn@gmail.com>
2022-04-11 15:54:26 -0300

    Criação do Banco de Dados. Status: Tudo okay

---
+++ CardGamesShop/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CardGamesShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CardGamesShop.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Card> Cards { get; set; }
+    }
+}
---
+++ CardGamesShop/Models/Cards.cs
@@ -0,0 +1,18 @@
+﻿namespace CardGamesShop.Models
+{
+    public class Card
+    {
+        public int CardId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsCardPreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        public int CatedoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
